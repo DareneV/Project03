7b5b3831e2838c3f0b73d166228ee26980b8576f
DareneV <dvelamac@u.rochester.edu>
2016-12-24 11:49:27 -0700

    'insert into' change

--- database-write.php
+++ database-write.php
@@ -10,10 +10,10 @@
 	$dbuser = "urcscon3_dvelama";
 	$dbpass = "coffee1N/!";
 	$dbname = "urcscon3_dvelama";
-	
+
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-	$query = "INSERT INTO contact (first_name, last_name, email, telephone, message) VALUES ('$firstname', '$lastname', '$email', '$telephone', '$message')";
+	$query = "INSERT INTO 'urcscon3_dvelama'.'contact' (first_name, last_name, email, telephone, message) VALUES ('$firstname', '$lastname', '$email', '$telephone', '$message')";
 
 	$result = mysqli_query($connection, $query);
 
